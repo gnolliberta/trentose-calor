743edadbc673f7294b6ec66feb442b5ea2275565
Giorgio <giopbm7@hotmail.it>
2015-12-07 17:24:27 +0100

    created switch between trento and rovereto

--- app.js
+++ app.js
@@ -18,13 +18,15 @@ $(document).ready(function(){
         data: data,
         dati: [],
         init: function(){
+            var tn=[]; var rv=[];
+            var j=0; var k=0;
             for(var i =0;i< this.data.length; i++){
-                var j=0; var k=0;
-                if(this.data[i].city == "Trento"){tn[j]=this.data[i]; j++;}
-                else{rv[k]=this.data[i]; k++;}
+                if(this.data[i].city == "trento"){ tn[j]=this.data[i]; j++; }
+                else{ rv[k]=this.data[i]; k++; }
             }
             this.dati[0] = tn;
             this.dati[1] = rv;
+            //console.log(this.dati[0] [1]);
         }
 
     };
@@ -36,32 +38,32 @@ $(document).ready(function(){
         summary: function(){
             View.summary();
         },
-        get_length: function(){
-            return Model.dati.length;
+        get_length: function(z){
+            return Model.dati[z].length;
         },
-        get_day: function(i){
-            return Model.dati[i].day;
+        get_day: function(z,i){
+            return Model.dati[z][i].day;
         },
-        get_period: function(i){
-            return Model.dati[i].period;
+        get_period: function(z,i){
+            return Model.dati[z][i].period;
         },
-        get_condition: function(i){
-            return Model.dati[i].condition;
+        get_condition: function(z,i){
+            return Model.dati[z][i].condition;
         },
-        get_max: function(current_day){
+        get_max: function(z,current_day){
             var max=0;
-            for(var i =0; i<Model.dati.length; i++){
-                if(Model.dati[i].day == current_day){
-                    if(max < Model.dati[i].temperature){max = Model.dati[i].temperature;}
+            for(var i =0; i<Model.dati[z].length; i++){
+                if(Model.dati[z][i].day == current_day){
+                    if(max < Model.dati[z][i].temperature){max = Model.dati[z][i].temperature;}
                 }
             }
             return max;
         },
-        get_min: function(current_day){
+        get_min: function(z,current_day){
             var min=1000;
-            for(var i =0; i<Model.dati.length; i++){
-                if(Model.dati[i].day == current_day){
-                    if(min > Model.dati[i].temperature){min = Model.dati[i].temperature;}
+            for(var i =0; i<Model.dati[z].length; i++){
+                if(Model.dati[z][i].day == current_day){
+                    if(min > Model.dati[z][i].temperature){min = Model.dati[z][i].temperature;}
                 }
             }
             return min;
@@ -70,18 +72,21 @@ $(document).ready(function(){
     };
   
     var View = {
-        
         summary: function(){
             $("#btn-filter").click(function(){
+                var z;
+                if($("select").val() == "trento"){z=0;}
+                else if($("select").val() == "rovereto"){z=1;}
                  $("#summary").html("");
-                 for(var i =0; i<Controller.get_length(); i++){
-                    if(i==0 || Controller.get_day(i-1)!=Controller.get_day(i)){
-                        console.log("giorno: ",Controller.get_day(i));
+                                console.log("z:",z);
+                 for(var i =0; i<Controller.get_length(z); i++){
+                    if(i==0 || Controller.get_day(z,i-1)!=Controller.get_day(z,i)){
+                        //console.log("giorno: ",Controller.get_day(z,i));
                         var tmpl = temp;
-                        tmpl = tmpl.replace("IMG", Controller.get_condition(i))
-                                    .replace("DAY", Controller.get_day(i))
-                                    .replace("MAX", Controller.get_max(Controller.get_day(i)))
-                                    .replace("MIN", Controller.get_min(Controller.get_day(i)));
+                        tmpl = tmpl.replace("IMG", Controller.get_condition(z,i))
+                                    .replace("DAY", Controller.get_day(z,i))
+                                    .replace("MAX", Controller.get_max(z,Controller.get_day(z,i)))
+                                    .replace("MIN", Controller.get_min(z,Controller.get_day(z,i)));
                         $("#summary").append(tmpl);
                     }
                 }
